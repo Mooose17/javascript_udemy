3c258532ed41026fe9eb8b22d5ec3babe26e46c9
Mooose17 <63780114+Mooose17@users.noreply.github.com>
2022-06-07 10:46:13 +0100

    If statement

---
+++ conditionals/app.js
@@ -0,0 +1,15 @@
+//If statement//
+let num = Math.random();
+
+if(num >= 0.5){
+    console.log("Num is greater than 0.5!")
+    console.log(num)
+}
+
+if(num <= 0.5){
+    console.log("Num is less than 0.5!")
+    console.log(num)
+}
+
+//----------------------------------------//
+//
