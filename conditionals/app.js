d4a0ddffa22e4e1de30cba83d83c8b50763cb0f2
Mooose17 <63780114+Mooose17@users.noreply.github.com>
2022-06-07 11:10:05 +0100

    If, Else If and Else - Comments in Code

--- conditionals/app.js
+++ conditionals/app.js
@@ -12,4 +12,46 @@ if(num <= 0.5){
 }
 
 //----------------------------------------//
-//
+//Else If statement//
+
+const age = 7;
+
+if(age < 5){
+    console.log("You are under 5. Free entry.")
+} else if(age<15){
+    console.log("You are a child. Pay £10 entry fee.")
+} else if(age<70){
+    console.log("You are ADULT. Pay £30 entry fee.")
+} else if(age>=71){
+    console.log("Senior discount - £10 entry fee.")
+};
+
+// Else if's are sequential and only one will run. //
+//----------------------------------------------------//
+// Else statements //  
+
+const dayOfWeek = "sdgf";
+
+if(dayOfWeek === "Monday"){
+    console.log("UGH Mondays are ass.")
+} else if (dayOfWeek === "Friday"){
+    console.log("FRIDAYFEELINGYESSSSSS")
+} else if (dayOfWeek === "Sunday"){
+    console.log("I dont wanna go to work tomorrow;-;")
+} else{
+    console.log("Day Of Week not seen.")
+}
+
+// Else is a catch all //
+//----------------------------//
+
+/*
+if - "If X returns true - Do this"
+
+if - "If X returns true - Do this"
+else if - "If the previous "if" doesnt return true - Do this"
+
+if - "If X returns true - Do this"
+else if - "If the previous "if" doesnt return true - Do this"
+else - "If nothing is returning true. Do this"
+*/
